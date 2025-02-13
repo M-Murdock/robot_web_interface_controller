686ca31392ff16d79c9e0cae4dbe956cd1a22395
Reuben M. Aronson <reuben.aronson@tufts.edu>
2025-02-13 17:13:28 -0500

    reorg for less hardcoded stuff

--- scripts/start_flask.py
+++ scripts/start_flask.py
@@ -1,12 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+import os
+from urllib.parse import urlparse
 
-app = Flask(__name__)
+
+filename = os.path.abspath(__file__)
+scriptsdir = os.path.dirname(filename)
+basedir = os.path.dirname(scriptsdir) 
+app = Flask(__name__, root_path=basedir)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', 
+        host=urlparse(request.host_url).hostname, 
+        port="9090")
 
-if __name__ == '__main__':  
-    app.run(host="10.42.99.11")
+if __name__ == '__main__': 
+    app.run(host="0.0.0.0")
     # app.run(debug="true")
